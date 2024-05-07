769bf97ec6b6559408bf7d0ad37bef45454a3244
Almix <111447259+Almixo@users.noreply.github.com>
2024-05-07 23:08:42 +0200

    Its leaking!!!

--- mp/src/game/server/CUSTOM/luger.cpp
+++ mp/src/game/server/CUSTOM/luger.cpp
@@ -1,4 +1,5 @@
 #include "cbase.h"
+#include "soundent.h"
 #include "hl1_basecombatweapon_shared.h"
 
 class CLuger : public CBaseHL1CombatWeapon
@@ -7,6 +8,8 @@ class CLuger : public CBaseHL1CombatWeapon
 	DECLARE_SERVERCLASS();
 public:
 	CLuger();
+
+	void PrimaryAttack( void );
 
 	Activity GetDrawActivity(void);
 	Activity GetPrimaryAttackActivity(void);
@@ -27,6 +30,58 @@ CLuger::CLuger()
 	m_bReloadsSingly = false;
 }
 
+void CLuger::PrimaryAttack( void )
+{
+	CBasePlayer *pPlayer = ToBasePlayer( GetOwner() );
+	if ( !pPlayer )
+		return;
+
+	if ( m_iClip1 <= 0 )
+	{
+		if ( !m_bFireOnEmpty )
+		{
+			Reload();
+		}
+		else
+		{
+			WeaponSound( EMPTY );
+			m_flNextPrimaryAttack = 0.15;
+		}
+
+		return;
+	}
+
+	WeaponSound( SINGLE );
+	pPlayer->DoMuzzleFlash();
+
+	m_flNextPrimaryAttack = gpGlobals->curtime + 0.20f;
+	m_flTimeWeaponIdle = gpGlobals->curtime + RandomInt( 2, 5 );
+
+	m_iClip1--;
+
+	SendWeaponAnim( GetPrimaryAttackActivity() );
+
+	pPlayer->SetAnimation( PLAYER_ATTACK1 );
+
+	Vector vecSrc = pPlayer->Weapon_ShootPosition();
+	Vector vecAiming = pPlayer->GetAutoaimVector( AUTOAIM_SCALE_DEFAULT );
+
+	pPlayer->SetMuzzleFlashTime( gpGlobals->curtime + 0.5 );
+
+	Vector vecSpread;
+
+	//Disorient the player
+	QAngle angles = pPlayer->EyeAngles();
+	angles.x -= 2 + RandomFloat( -0.5f, 0.5f );
+	vecSpread = Vector( 0.01f, 0.01f, 0.01f );
+
+	pPlayer->SnapEyeAngles( angles );
+
+	pPlayer->FireBullets( 1, vecSrc, vecAiming, vecSpread, MAX_TRACE_LENGTH, m_iPrimaryAmmoType, 3 );
+
+	CSoundEnt::InsertSound( SOUND_COMBAT, GetAbsOrigin(), 600, 0.2, GetOwner() );
+}
+
 Activity CLuger::GetDrawActivity(void)
 {
 	if (m_iClip1 <= 0)
@@ -37,7 +92,7 @@ Activity CLuger::GetDrawActivity(void)
 
 Activity CLuger::GetPrimaryAttackActivity(void)
 {
-	if ((m_iClip1 - 1) <= 0)
+	if (m_iClip1 <= 0)
 		return ACT_GLOCK_SHOOTEMPTY;
 	else
 		return ACT_VM_PRIMARYATTACK;
